bdf2bac7ac02834928f22242d7f4fa1091d47765
lcvaz <lucasvaz278@gmail.com>
2025-12-02 21:57:30 -0300

    primeiro commit branch develop

---
+++ Domain/Entities/Avaliacao.cs
@@ -0,0 +1,19 @@
+namespace Domain.Entities;
+
+public class Avaliacao : Entity
+{
+    ///<summary>
+    /// Uma avaliação terá um padrão de suportar até 5 fotos , um texto de 1000 caracteres 
+    /// e uma avalização de 0 a 5  
+    ///</summary> 
+     
+    public FotosAvaliacao Fotos { get; set; } = 
+    public int MyProperty { get; set; }
+    public int MyProperty { get; set; }
+
+
+    public Avaliacao()
+    {
+        
+    }
+}
---
+++ Domain/FotosAvaliacao.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entities;
+
+public class FotosAvaliacao
+{
+    ///<summary>
+    /// Este é o ValueObject pois uma foto ou varias para uma avaliacao é um objeto complexo e
+    /// são salvos junto com a entidade no banco de dados
+    ///</summary> 
+     
+    public int MyProperty { get; set; }
+    public int MyProperty { get; set; }
+    public int MyProperty { get; set; }
+
+
+    public FotosAvaliacao()
+    {
+        
+    }
+}
+
