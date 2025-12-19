9580f1378a850ca984015967d80384785afe04a7
lcvaz <lucasvaz278@gmail.com>
2025-12-19 09:44:48 -0300

    estrutura frontend core, shared, guards, store, features e layouts

---
+++ ProjetoBraboWEB/src/app/features/vendedor/vendedor-home/vendedor-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendedor-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './vendedor-home.component.html',
+  styleUrl: './vendedor-home.component.scss'
+})
+export class VendedorHomeComponent {
+
+}
---
+++ ProjetoBraboWEB/src/app/layouts/cliente-layout/cliente-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './cliente-layout.component.html',
+  styleUrl: './cliente-layout.component.scss'
+})
+export class ClienteLayoutComponent {
+
+}
