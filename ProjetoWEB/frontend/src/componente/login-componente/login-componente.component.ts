061b3e8cd08fdf5dd390cf2dbea4ec0ac66f7aee
lcvaz <lucasvaz278@gmail.com>
2026-01-08 19:46:05 -0300

    reiniciando

---
+++ ProjetoWEB/frontend/src/componente/login-componente/login-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-componente.component.html',
+  styleUrl: './login-componente.component.scss'
+})
+export class LoginComponenteComponent {
+
+}
